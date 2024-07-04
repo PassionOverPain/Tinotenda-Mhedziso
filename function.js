decf18da56f4d0f7022c31114dabd9eb6ae6f6fa
Tino <tinomhedziso21@gmail.com>
2024-07-05 00:30:14 +0200

    Update

--- function.js
+++ function.js
@@ -6,3 +6,18 @@ menuToggle.onclick = function()
 } 
 let text = document.querySelector('.myName');
 text.innerHTML = text.innerText.split('').map((letters, i) => `<span style="transition-delay:${ i * 40}ms;">${letters}</span>`).join('');
+var viewpoints = document.getElementsByClassName('Viewpoint');
+var views = document.getElementsByClassName('View');
+function show(thisview)
+{
+for(viewpoint of viewpoints)
+    {
+        viewpoint.classList.remove("Activepoint");
+    }
+    for(view of views)
+        {
+            view.classList.remove("Activeview");
+        }
+    event.currentTarget.classList.add("Activepoint");
+    document.getElementById('View').classList.add("Activeview");
+}
