f4fc6dcc9db3d1d74b55ac73737fba4cf56c5749
Tino <tinomhedziso21@gmail.com>
2024-07-21 20:46:13 +0200

    Update

--- function.js
+++ function.js
@@ -5,14 +5,6 @@ let menu = document.querySelector(".menu");
 menuToggle.onclick = function () {
   menu.classList.toggle("active");
 };
-let text = document.querySelector(".myName");
-text.innerHTML = text.innerText
-  .split("")
-  .map(
-    (letters, i) =>
-      `<span style="transition-delay:${i * 40}ms;">${letters}</span>`
-  )
-  .join("");
 var viewpoints = document.getElementsByClassName("Viewpoint");
 var views = document.getElementsByClassName("View");
 function show(thingy) {
